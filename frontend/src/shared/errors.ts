e4f986b5b4141a7e63f9433b5973a11b5b6ce30c
Darya V <57266356+DaryaVkh@users.noreply.github.com>
2023-04-28 21:28:06 +0500

    clean repository

--- frontend/src/shared/errors.ts
+++
@@ -1,7 +0,0 @@
-export const REQUIRED_CONTROL_ERROR = 'Поле обязательно для заполнения';
-export const EMAIL_CONTROL_ERROR = 'Должен быть введен валидный email';
-
-export enum ErrorCode {
-  REQUIRED = 'required',
-  EMAIL = 'email',
-}
